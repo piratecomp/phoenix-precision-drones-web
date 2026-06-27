cf13861daaf3d676984564e9388187ea6ca137a1
piratecomp <piratecomp@icloud.com>
2026-06-26 18:42:22 -0700

    Remove stacked owner portal gateway panel

--- app/portal/owner/page.tsx
+++ app/portal/owner/page.tsx
@@ -1,13 +1,7 @@
 import { PortalDashboardDataShell } from "@/components/PortalDataShell";
-import AIGatewayPanel from "@/components/AIGatewayPanel";
 
 export default function PortalDashboardPage() {
   return (
-    <>
-      <PortalDashboardDataShell dashboardKey="owner" />
-      <div className="portal-main-area maintenance-bridge-inline-host">
-        <AIGatewayPanel />
-      </div>
-    </>
+    <PortalDashboardDataShell dashboardKey="owner" />
   );
 }
