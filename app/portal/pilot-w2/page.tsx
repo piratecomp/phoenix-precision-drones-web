7d0ad2399b645610e9f7e3e2db1659298a078115
piratecomp <piratecomp@icloud.com>
2026-07-03 12:43:43 -0700

    Add command workspace to W2 pilot dashboard

--- app/portal/pilot-w2/page.tsx
+++ app/portal/pilot-w2/page.tsx
@@ -1,5 +1,11 @@
+import DashboardCommandWorkspace from "@/components/DashboardCommandWorkspace";
 import { PortalDashboardDataShell } from "@/components/PortalDataShell";
 
 export default function PortalDashboardPage() {
-  return <PortalDashboardDataShell dashboardKey="pilot_w2" />;
+  return (
+    <>
+      <PortalDashboardDataShell dashboardKey="pilot_w2" />
+      <DashboardCommandWorkspace dashboardKey="pilot_w2" />
+    </>
+  );
 }
