136c261b51002c0b01012e51fdfea4e21fe325e2
piratecomp <piratecomp@icloud.com>
2026-07-03 12:44:40 -0700

    Add command workspace to customer dashboard

--- app/portal/customer/page.tsx
+++ app/portal/customer/page.tsx
@@ -1,5 +1,11 @@
+import DashboardCommandWorkspace from "@/components/DashboardCommandWorkspace";
 import { PortalDashboardDataShell } from "@/components/PortalDataShell";
 
 export default function PortalDashboardPage() {
-  return <PortalDashboardDataShell dashboardKey="customer" />;
+  return (
+    <>
+      <PortalDashboardDataShell dashboardKey="customer" />
+      <DashboardCommandWorkspace dashboardKey="customer" />
+    </>
+  );
 }
