6308bd63e9db7bb22d2c738cb8068cd317322259
piratecomp <piratecomp@icloud.com>
2026-07-03 12:43:22 -0700

    Add command workspace to sales dashboard

--- app/portal/sales/page.tsx
+++ app/portal/sales/page.tsx
@@ -1,5 +1,11 @@
+import DashboardCommandWorkspace from "@/components/DashboardCommandWorkspace";
 import { PortalDashboardDataShell } from "@/components/PortalDataShell";
 
 export default function PortalDashboardPage() {
-  return <PortalDashboardDataShell dashboardKey="sales_marketing" />;
+  return (
+    <>
+      <PortalDashboardDataShell dashboardKey="sales_marketing" />
+      <DashboardCommandWorkspace dashboardKey="sales_marketing" />
+    </>
+  );
 }
